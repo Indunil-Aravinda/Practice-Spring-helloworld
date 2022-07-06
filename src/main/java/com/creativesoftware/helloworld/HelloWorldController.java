1772d577dabba851fba4301f75f89a7f9b21e617
Indunil Aravinda (Creative Software) <Indunil.A@CreativeSoftware.com>
2022-07-06 06:54:35 +0530

    String text test

---
+++ src/main/java/com/creativesoftware/helloworld/HelloWorldController.java
@@ -0,0 +1,4 @@
+package com.creativesoftware.helloworld;
+
+public class HelloWorldController {
+}
--- src/test/java/com/creativesoftware/helloworld/CheckHTTPResponse.java
+++ src/test/java/com/creativesoftware/helloworld/CheckHTTPResponse.java
@@ -1,12 +1,31 @@
 package com.creativesoftware.helloworld;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CheckHTTPResponse {
+
+//    get the random port number, then the testing framework can inject a random port to
+//    local server port
     @LocalServerPort
     private int port;
 
-    
+//    get a test rest template from the context
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+//    In the following test case, it passes if the string that we put in the
+//    controller is same as the string served at the localhost random port
+//    "Hello World works perfectly" is what is put on the controller
+    @Test
+    public void shouldPassIfStringMatches(){
+        assertEquals("Hello World works perfectly",
+                testRestTemplate.getForObject("http://localhost:"+port+"/", String.class ));
+    }
 }
