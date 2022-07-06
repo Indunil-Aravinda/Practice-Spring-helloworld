b46bd2a2d77fd27f05d4ff9ad241ae809983953f
Indunil Aravinda (Creative Software) <Indunil.A@CreativeSoftware.com>
2022-07-06 07:07:26 +0530

    Hello World Controllder done and tested -get the expected outcome

--- src/main/java/com/creativesoftware/helloworld/HelloWorldController.java
+++ src/main/java/com/creativesoftware/helloworld/HelloWorldController.java
@@ -1,4 +1,12 @@
 package com.creativesoftware.helloworld;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
+    @RequestMapping
+    public String helloWorld(){
+        return "Hello World works perfectly";
+    }
 }
